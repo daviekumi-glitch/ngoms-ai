38dd8eb28e8752ca63f27cc611c42c558a031b1f
daviekumi-glitch <daviekumi@gmail.com>
2026-07-22 15:19:26 +0200

    fix: vercel build errors - remove dark class, fix imports, seed data format, code splitting

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { Toaster } from 'react-hot-toast'
 import App from './App'
 import './styles/globals.css'
 
@@ -9,9 +8,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
-      <Toaster position="top-right" toastOptions={{
-        style: { background:'#0F1629', color:'#F8FAFC', border:'1px solid rgba(59,130,246,0.3)' }
-      }} />
     </BrowserRouter>
   </React.StrictMode>
 )
